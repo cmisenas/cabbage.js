bd3712e46fac71ff9ed3aa1d1212ad4851890275
Jade Misenas <acolyte.camille@gmail.com>
2015-07-20 17:54:53 -0400

    Start writing tests (this will fail if tests are ran)
    - So I broke the golden tdd rule and wrote code before
      tests but only because I started out with existing
      code extracted from earlier projects
    - Add more testing frameworks and libs
      * chai
      * sinon-chai
    - Add script to run tests

--- test/cabbage.js
+++ test/cabbage.js
@@ -1,29 +1,54 @@
-var assert = require('assert'),
+var chai = require('chai'),
     sinon = require('sinon'),
+    sinonChai = require('sinon-chai'),
+    expect = chai.expect,
     jsdom = require('jsdom').jsdom,
     document = jsdom('<html></html>', {}),
     window = document.defaultView;
 
 var Cabbage = require('../js/cabbage').Cabbage,
-    canvas;
+    Pixel = require('../js/pixel').Pixel,
+    cabbage;
+
+global.Cabbage = Cabbage;
+global.Pixel = Pixel;
+
+chai.use(sinonChai);
 
 describe('Cabbage', function(){
-  setup(function(){
-    document.getElementById = sinon.stub().returns({
-      getContext: function(){
-        return {
-          putImageData: sinon.spy(),
-          getImageData: sinon.spy(),
-          createImageData: sinon.spy(),
-          fillRect: sinon.spy()
-        };
-      }
+  describe('initialization', function() {
+    setup(function(){
+      document.getElementById = sinon.stub().returns({
+        getContext: function(arg){
+          return {
+            drawImage: sinon.spy(),
+            putImageData: sinon.spy(),
+            getImageData: sinon.spy(),
+            createImageData: sinon.spy(),
+            fillRect: sinon.spy()
+          };
+        }
+      });
+      cabbage = new Cabbage('foo', 10, 10, document);
+      // bypass _creatImage functionality which simply loads image via HTMLImageElement
+      sinon.stub(cabbage, '_createImage', function(src, fn) {
+        fn(src);
+      });
     });
-    canvas = new Cabbage('foo', 10, 10, document);
-  });
 
-  describe('loadImg', function() {
-    it('uses the x and y coordinates if provided', function() {
+    describe('loadImg', function() {
+      it('uses the x and y coordinates if provided', function() {
+        var x = 5, y = 20;
+        var imgSrc = 'foo.jpg';
+        cabbage.loadImg(imgSrc, x, y);
+        expect(cabbage.ctx.drawImage).to.have.been.calledWith(imgSrc, x, y);
+      });
+
+      it('draws image at the top left corner if no x and y coordinates provided', function() {
+        var imgSrc = 'foo.jpg';
+        cabbage.loadImg(imgSrc);
+        expect(cabbage.ctx.drawImage).to.have.been.calledWith(imgSrc, 0, 0);
+      });
     });
   });
 
