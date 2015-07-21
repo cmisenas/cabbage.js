56469c09409408fbb3d10ce4622cf89f67a8f2ea
Jade Misenas <acolyte.camille@gmail.com>
2015-07-21 11:51:18 -0400

    Make the helpers tests pass
    - Also corrected `getPixel` which uses the conversion helpers
      Removed default value and just expect an error thrown at you

--- js/cabbage.js
+++ js/cabbage.js
@@ -96,34 +96,30 @@
       for (var x = 0; x < this.width; x++) {
         pixelIndex = (y * this.width) + x;
         cvsIndex = x * 4 + y * this.width * 4;
-        matrix = this._getMatrix(x, y, size);
+        matrix = this._buildMatrix(x, y, size);
         fn(matrix, x, y, pixelIndex, cvsIndex);
       }
     }
   };
 
-  // returns the default value (if defined, otherwise null)
-  // or a pixel object
-  Cabbage.prototype.getPixel = function(loc, defVal) {
+  // returns the pixel object if it exists
+  // otherwise throws an error
+  Cabbage.prototype.getPixel = function(loc) {
     var index, coords;
 
     if (typeof loc === 'number') {
       index = loc;
-      coords = this._convertToCoords(loc);
+      coords = this._convertIDIndexToCoords(loc);
     } else {
-      index =  this._convertToIndex(loc);
+      index =  this._convertCoordsToIDIndex(loc);
       coords = loc;
     }
 
-    if (index < 0 || index > (this.currImg.data.length - 1)) {
-      return defVal;
-    } else {
-      return new Pixel(coords.x, coords.y, this.currImg.data.slice(index, index + 4));
-    }
+    return new Pixel(coords.x, coords.y, this.currImg.data.slice(index, index + 4));
   };
 
   Cabbage.prototype.setPixel = function(pixel, val) {
-    var i = this._convertToIndex(pixel),
+    var i = this._convertCoordsToIDIndex(pixel),
         r, g, b, a;
 
     if (typeof val === 'number') {
@@ -157,19 +153,6 @@
            coords.y > this.height - 1;
   }
 
-  Cabbage.prototype._convertToIndex = function(coords) {
-    var m = 4;
-    return (coords.x * m) + (coords.y * this.width * m);
-  };
-
-  Cabbage.prototype._convertToCoords = function(index) {
-    var m = 4;
-    return {
-            x : (index % (this.width * m)) / m,
-            y : Math.floor(index / (this.width * m))
-           };
-  };
-
   // Every putImageData done via object
   // stores current image for faster access later
   Cabbage.prototype.putImageData = function(imgData, x, y) {
@@ -181,15 +164,96 @@
     this.currImg = this.getCurrentImg();
   };
 
-  Cabbage.prototype._getMatrix = function(cx, cy, size) {
-    var matrix = [];
+  Cabbage.prototype._buildMatrix = function(cx, cy, size) {
+    var matrix = [], nx, ny;
+    var min = 3, max = Math.max(this.width, this.height); // temp max value
+    var size = size || 3;
+    size = (size % 2 === 0) ? size + 1 : size;
+    // TODO make it so that max size is dictated by the dimensions of the image
+    if (size < min || size > max) size = min;
+
     for (var i = 0, y = -(size-1)/2; i < size; i++, y++) {
       matrix[i] = [];
       for (var j = 0, x = -(size-1)/2; j < size; j++, x++) {
-        matrix[i][j] = (cx + x) * 4 + (cy + y) * that.width * 4;
+        nx = cx + x;
+        ny = cy + y;
+        if (nx < 0 || nx >= this.width || ny < 0 || ny >= this.height) {
+          matrix[i][j] = undefined;
+        } else {
+          matrix[i][j] = this._convertCoordsToIDIndex({x: nx, y: ny});
+        }
       }
     }
     return matrix;
+  };
+
+  Cabbage.prototype._convertCoordsToIDIndex = function(coords) {
+    var msg = 'Invalid coordinate. Unable to convert to image data index';
+    this._checkValidCoords(coords, msg);
+    var m = 4;
+    return (coords.y * this.width + coords.x) * m;
+  };
+
+  Cabbage.prototype._convertCoordsToPixIndex = function(coords) {
+    var msg = 'Invalid coordinate. Unable to convert to pixel index';
+    this._checkValidCoords(coords, msg);
+    return (coords.y * this.width) + coords.x;
+  };
+
+  Cabbage.prototype._checkValidCoords = function(coords, msg) {
+    if (!coords ||
+        !(coords.x === parseInt(coords.x, 10)) ||
+        !(coords.y === parseInt(coords.y, 10)) ||
+        coords.x < 0 || coords.x >= this.width ||
+        coords.y < 0 || coords.y >= this.height) {
+      throw new Error(msg);
+    }
+  };
+
+  Cabbage.prototype._checkValidPIndex = function(pIdx, msg) {
+    if (!(pIdx === parseInt(pIdx, 10)) ||
+        pIdx < 0 ||
+        pIdx >= (this.width * this.height)) {
+      throw new Error(msg);
+    }
+  };
+
+  Cabbage.prototype._checkValidIDIndex = function(pIdx, msg) {
+    if (!(pIdx === parseInt(pIdx, 10)) ||
+        pIdx < 0 ||
+        pIdx >= (this.width * this.height * 4)) {
+      throw new Error(msg);
+    }
+  };
+
+  Cabbage.prototype._convertIDIndexToCoords = function(idIdx) {
+    var msg = 'Invalid image data index. Unable to convert to coordinates';
+    var m = 4;
+    this._checkValidIDIndex(idIdx, msg);
+    if (idIdx % 4 > 0) idIdx -= idIdx % 4;
+    return {
+            x : (idIdx % (this.width * m)) / m,
+            y : Math.floor(idIdx / (this.width * m))
+           };
+  };
+
+  Cabbage.prototype._convertIDIndexToPixIndex = function(idIdx) {
+    var msg = 'Invalid image data index. Unable to convert to pixel index';
+    var m = 4;
+    this._checkValidIDIndex(idIdx, msg);
+    return Math.floor(idIdx/m);
+  };
+
+  Cabbage.prototype._convertPixIndexToCoords = function(pIdx) {
+    var msg = 'Invalid pixel index. Unable to convert to coordinates';
+    this._checkValidPIndex(pIdx, msg);
+    return { x: pIdx%this.width, y: Math.floor(pIdx/this.width) };
+  };
+
+  Cabbage.prototype._convertPixIndexToIDIndex = function(pIdx) {
+    var msg = 'Invalid pixel index. Unable to convert to image data index';
+    this._checkValidPIndex(pIdx, msg);
+    return pIdx * 4;
   };
 
   exports.Cabbage = Cabbage;
